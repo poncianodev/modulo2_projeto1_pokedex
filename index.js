f99b09848f41664b0f088d582b5389be3cef1077
poncianodev <lucasbponciano@hotmail.com>
2022-04-25 20:49:57 -0300

    adição do cadastro de pokemons

--- index.js
+++ index.js
@@ -5,37 +5,64 @@ const app = express();
 
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
-
+app.use(express.urlencoded());
 
 const pokedex = [
     {
         id: 1,
-        name: "Bulbassauro",
-        description: "There is a plant seed on its back right from the day this Pokémon is born. The seed slowly grows larger.",
-        type: "Type: Grass",
-        image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png"
+        name: "Bulbassaur",
+        description:
+            "There is a plant seed on its back right from the day this Pokémon is born. The seed slowly grows larger.",
+        type: "Grass",
+        image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png",
     },
     {
         id: 2,
         name: "Charmander",
-        description: "It has a preference for hot things. When it rains, steam is said to spout from the tip of its tail.",
-        type: "Type: Fire",
-        image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png"
+        description:
+            "It has a preference for hot things. When it rains, steam is said to spout from the tip of its tail.",
+        type: "Fire",
+        image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png",
     },
     {
         id: 3,
         name: "Squirtle",
-        description: "When it retracts its long neck into its shell, it squirts out water with vigorous force.",
-        type: "Type: Water",
-        image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png"
+        description:
+            "When it retracts its long neck into its shell, it squirts out water with vigorous force.",
+        type: "Water",
+        image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png",
     },
-]
+];
 
-app.get("/", (req, res) => {
-    res.render("index", {pokedex});
-});
+let pokemon = undefined;
 
 //Rotas
+app.get("/", (req, res) => {
+    res.render("index", { pokedex, pokemon });
+});
+
+app.post("/add", (req, res) => {
+    const pokemon = req.body;
+    pokemon.id = pokedex.length + 1;
+    pokedex.push(pokemon);
+    res.redirect("/");
+});
+
+app.get("/details/:id", (req, res) => {
+    const id = +req.params.id;
+    pokemon = pokedex.find((pokemon) => pokemon.id === id);
+    res.redirect("/");
+});
+
+app.post("/update/:id", (req, res) => {
+    const id = +req.params.id - 1;
+    const newPokemon = req.body;
+    newPokemon.id = id + 1;
+    pokedex[id] = newPokemon;
+    pokemon = undefined;
+    res.redirect("/");
+});
+
 app.listen(3000, () =>
     console.log("Servidor rodando em http://localhost:3000"),
 );
